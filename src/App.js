8c5c1ec6cb39e55a1743697a27f1f27b8943d127
Tylyps <j.chmielewski@o2.pl>
2018-11-13 20:57:11 +0100

    Create new Routes path

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-
 const appRoot = document.getElementById("app");
 
-ReactDOM.render(<p>This is a Boilerplate</p>, appRoot);
+const ExpenseDashboardPage = () => (
+  <div>
+    <h1>This is from my ExpenseDashboardPage component</h1>
+  </div>
+);
+
+const AddExpansePage = () => (
+  <div>
+    <h1>This is from my AddExpansePage component</h1>
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    <h2>This is from my EditExpensePage component</h2>
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    <h2>This is from my HelpPage component</h2>
+  </div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact />
+      <Route path="/create" component={AddExpansePage} exact />
+      <Route path="/edit" component={EditExpensePage} exact />
+      <Route path="/help" component={HelpPage} exact />
+    </div>
+  </BrowserRouter>
+);
+
+
+ReactDOM.render(routes, appRoot);
